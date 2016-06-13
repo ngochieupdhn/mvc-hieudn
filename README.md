b4ff9d300376f7ddcef7adb5d8a1c49dd8837dc2
ngochieupdhn <dongochieu.tink4a1@gmail.com>
2016-06-13 11:42:25 +0700

    laravel

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mvc-hoangn
+# Laravel - Hiếu
+
+## Database
+
+	change database name in .env
+		DB_DATABASE = laravel
+
+## Run
+
+	php artisan migrate
+
+	php artisan make:seeder
+
+## Login
+
+	Email: admin@gmail.com
+
+	Password: 123456
--- index.php
+++ index.php
@@ -1,76 +1,58 @@
-<?php 
-session_start();
-define('PATH', dirname(__FILE__));
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Đăng Nhập</title>
-	<link rel="stylesheet" type="text/css" href="public/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="public/css/bootstrap-theme.min.css">
-</head>
-<body>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="public/js/npm.js"></script>
-</body>
-</html>
-
 <?php
-if(!isset($_SESSION['dangnhap']))
-{
-	if(isset($_GET['controller']))
-	{
-		$controller = $_GET['controller'];
-		if($controller == 'user')
-		{
-			$filename = $controller.'controller.php';
-			require_once PATH.'/controller/'.$filename;
-			if(isset($_GET['function']))
-			{
-				$function = $_GET['function'];
 
-			}
-			else
-			{
-				$function = 'login';
-			}
-			$a = new $controller;
-			$a->$function();
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
 
-		}
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
 
-	}
-	else
-		{
-			header('location:index.php?controller=user');
-		}
-		
-}
-else
-{
-	if(isset($_GET['controller']))
-	{
-		$controller = $_GET['controller'];
-	}
-	else
-	{
-		$controller = 'tintuc';
-	}
-	$filename = $controller.'controller.php';
-	require_once PATH.'/controller/'.$filename;
-	if(isset($_GET['function']))
-	{
-		$function = $_GET['function'];
-	}
-	else
-	{
-		$function = 'allnews';
-	}
-	$a = new $controller;
-	$a->$function();
-}
+require __DIR__.'/bootstrap/autoload.php';
 
-?>
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
